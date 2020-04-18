16f0c7ee05dea080bb22f7022593a82fad61cec6
gtafiper <onechr@gmail.com>
2020-04-18 21:18:49 +0200

    fjernet den ene knap

--- app/src/main/java/com/example/feast/MainActivity.java
+++ app/src/main/java/com/example/feast/MainActivity.java
@@ -13,14 +13,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Button btnLetsFeast = (Button) findViewById(R.id.btnLetsFeast);
-
-        btnLetsFeast.setOnClickListener(new View.OnClickListener() {
-           @Override
-           public void onClick(View v) {
-               Toast.makeText(v.getContext(), "i want toast", Toast.LENGTH_LONG).show();
-           }
-        });
     }
 }
