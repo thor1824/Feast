7fbbb88c33e627463dcbde4d33f6fb2b48b5aa07
gtafiper <onechr@gmail.com>
2020-05-10 21:58:34 +0200

    spin

--- app/src/main/java/com/example/feast/MainActivity.java
+++ app/src/main/java/com/example/feast/MainActivity.java
@@ -4,14 +4,44 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.varunest.sparkbutton.SparkButton;
 
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+    Spinner mainSpinner;
+    SparkButton sparkButton;
+    String valueFromSpinner;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mainSpinner = findViewById(R.id.spinnerMain);
+        sparkButton = findViewById(R.id.spark_button);
+        String[] spinnerValues = getResources().getStringArray(R.array.minutesForSpinner);
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, spinnerValues);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mainSpinner.setAdapter(adapter);
+
     }
+
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        if (parent.getId() == R.id.spinnerMain) {
+           this.valueFromSpinner = parent.getItemAtPosition(position).toString();
+
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+
 }
