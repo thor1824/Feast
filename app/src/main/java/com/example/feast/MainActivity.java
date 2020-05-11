32508f8cddd6e479c68101cf46fc9f1a9ec37293
nijashansen <nijas.w.hansen@gmail.com>
2020-05-11 12:01:29 +0200

    update

--- app/src/main/java/com/example/feast/MainActivity.java
+++ app/src/main/java/com/example/feast/MainActivity.java
@@ -2,17 +2,20 @@ package com.example.feast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.varunest.sparkbutton.SparkButton;
 
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
     Spinner mainSpinner;
     SparkButton sparkButton;
     String valueFromSpinner;
@@ -23,6 +26,13 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
         mainSpinner = findViewById(R.id.spinnerMain);
         sparkButton = findViewById(R.id.spark_button);
+        sparkButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                goToNextActivity();
+            }
+        });
         String[] spinnerValues = getResources().getStringArray(R.array.minutesForSpinner);
         ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, spinnerValues);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
@@ -42,6 +52,14 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     @Override
     public void onNothingSelected(AdapterView<?> parent) {
 
+    }
+
+    public void goToNextActivity() {
+        Intent intent = new Intent(this, DisplayRecipe.class);
+        mainSpinner = findViewById(R.id.spinnerMain);
+        String message = mainSpinner.getSelectedItem().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 
 }
