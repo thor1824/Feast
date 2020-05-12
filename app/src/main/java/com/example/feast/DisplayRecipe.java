96dc0b04afd2146734e8b69b033f17dbf1f0da5a
nijashansen <nijas.w.hansen@gmail.com>
2020-05-12 15:45:05 +0200

    Update DisplayRecipe.java

--- app/src/main/java/com/example/feast/DisplayRecipe.java
+++ app/src/main/java/com/example/feast/DisplayRecipe.java
@@ -2,6 +2,7 @@ package com.example.feast;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
