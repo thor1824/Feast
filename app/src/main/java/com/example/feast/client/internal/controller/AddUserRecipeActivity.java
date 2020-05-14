2717d557098e313e9298794e042e8e6a96eb55dd
gtafiper <onechr@gmail.com>
2020-05-14 09:49:17 +0200

    add

--- app/src/main/java/com/example/feast/client/internal/controller/AddUserRecipeActivity.java
+++ app/src/main/java/com/example/feast/client/internal/controller/AddUserRecipeActivity.java
@@ -1,23 +1,182 @@
 package com.example.feast.client.internal.controller;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputType;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import com.example.feast.R;
+import com.example.feast.core.entities.Ingredient;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.navigation.NavigationView;
+
+import java.util.ArrayList;
 
 public class AddUserRecipeActivity extends AppCompatActivity {
 
     DrawerLayout drawerLayout;
     NavigationView navigationView;
     Toolbar toolbar;
+    TextView ingText;
+    LinearLayout addIngLayout;
+    ScrollView scrollView;
+    EditText recipeNameField;
+    EditText estimatedTimeField;
+    EditText firstIngFiled;
+    EditText firstAmountField;
+    FloatingActionButton button;
+    int idName = 0;
+    int idAmount = 0;
+    int idContainer = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_user_recipe);
+
+        //-------------------instantiate-------------------\\
+        drawerLayout = findViewById(R.id.drawLayout_addRecipe);
+        navigationView = findViewById(R.id.navigation_view_addRecipe);
+        toolbar = findViewById(R.id.toolbar);
+        ingText = findViewById(R.id.textIng);
+        addIngLayout = findViewById(R.id.addIngLayout);
+        recipeNameField = findViewById(R.id.editName);
+        estimatedTimeField = findViewById(R.id.editTime);
+        firstIngFiled = findViewById(R.id.editIng);
+        firstAmountField = findViewById(R.id.editIngAmount);
+        button = findViewById(R.id.addIngButton);
+
+        //---------------------------------------------------\\
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addIngIngredient();
+            }
+        });
     }
+
+    public void addIngIngredient() {
+        LinearLayout.LayoutParams p = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+
+        idContainer++;
+        final LinearLayout editContainer = new LinearLayout(this);
+        editContainer.setId(idContainer);
+        editContainer.setLayoutParams(p);
+        editContainer.setOrientation(LinearLayout.HORIZONTAL);
+        addIngLayout.addView(editContainer);
+
+
+        idName++;
+        final EditText ingName = new EditText(this);
+        ingName.setHint("Ingredient");
+        ingName.setLayoutParams(p);
+        ingName.setId(idName);
+        ingName.setLayoutParams(new LinearLayout.LayoutParams(600, LinearLayout.LayoutParams.WRAP_CONTENT));
+        editContainer.addView(ingName);
+
+        idAmount++;
+        final EditText ingAmount = new EditText(this);
+        ingAmount.setHint("Amount");
+        ingAmount.setInputType(InputType.TYPE_CLASS_NUMBER);
+        ingAmount.setLayoutParams(p);
+        ingAmount.setId(idAmount);
+        LinearLayout.LayoutParams p2 = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        p2.leftMargin = 30;
+        ingAmount.setLayoutParams(p2);
+        editContainer.addView(ingAmount);
+
+
+        FloatingActionButton deleteIngButton = new FloatingActionButton(this);
+        deleteIngButton.setImageResource(R.drawable.delete_icon);
+        LinearLayout.LayoutParams p3 = (new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+        p3.leftMargin = 30;
+        deleteIngButton.setLayoutParams(p3);
+        deleteIngButton.setCompatElevation(0);
+        editContainer.addView(deleteIngButton);
+
+        deleteIngButton.setOnClickListener((new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addIngLayout.removeView(editContainer);
+
+            }
+        }));
+
+
+    }
+
+
+    private void saveUserRecipe() {
+        ArrayList<Ingredient> listOfIng = new ArrayList<Ingredient>();
+
+
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        setSupportActionBar(toolbar);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.nav_home:
+                Intent home_intent = new Intent(this, MainActivity.class);
+                startActivity(home_intent);
+                break;
+            case R.id.nav_addRecipe:
+                Intent recipe_intent = new Intent(this, RecipesActivity.class);
+                startActivity(recipe_intent);
+                break;
+            case R.id.nav_profile:
+                Intent profile_intent = new Intent(this, ProfileActivity.class);
+                startActivity(profile_intent);
+                break;
+
+            case R.id.nav_settings:
+                Toast.makeText(this, "We Have No Settings", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.nav_contact:
+                Toast.makeText(this, "We Can't Be Contacted", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.nav_rating:
+                Toast.makeText(this, "You Have Rated Us 5 Stars. Thank You <3", Toast.LENGTH_SHORT).show();
+                break;
+
+        }
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
 }
